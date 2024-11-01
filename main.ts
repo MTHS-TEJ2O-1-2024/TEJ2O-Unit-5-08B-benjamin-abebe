3230610e0b7298029f2c411e56cff961591c9f00
benjamin-abebe <benjamin.abebe@mths.ca>
2024-11-01 11:27:12 -0400

    Edited the typescript file.

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Benjamin Abebe
+ * Created on: Oct 2024
+ * This program moves the motor and checks If theres an object nearby
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+
+let distanceOfObject: number
+
+// loop forever
+while (true) {
+    distanceOfObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    // if distance of object is less than 10
+    if (distanceOfObject < 10) {
+        robotbit.StpCarMove(0, 0)
+        basic.showIcon(IconNames.No)
+        robotbit.StpCarMove(-10, 65)
+    } else {
+        robotbit.StpCarMove(10, 65)
+        basic.showIcon(IconNames.Yes)
+    }
+}
